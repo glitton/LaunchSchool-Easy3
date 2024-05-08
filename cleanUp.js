9e71f34545699097545a7364331f09012e61691c
Generosa Litton <glcodeworks@gmail.com>
2024-05-08 15:32:16 -0500

    copied solution to cleanup, need to revisit

--- cleanUp.js
+++ cleanUp.js
@@ -1,5 +1,39 @@
-function cleanUp(string) {
-  return string.replace(/[^a-zA-Z0-9]/g, " ");
+// function cleanUp(string) {
+//   const cleanText = string.replace(/[^a-zA-Z0-9]/g, " ");
+//   let cleanTextNoSpaces = "";
+//   for (let i = 0; i < cleanText.length; i++) {
+//    if(cleanText[i] === ' ')
+//   }
+// }
+
+// console.log(cleanUp("---what's my +*& line?"));
+
+function cleanUp(text) {
+  let cleanText = "";
+
+  for (let idx = 0; idx < text.length; idx += 1) {
+    if (isLowerCaseLetter(text[idx]) || isUpperCaseLetter(text[idx])) {
+      cleanText += text[idx];
+    } else if (idx === 0 || lastChar(cleanText) !== " ") {
+      cleanText += " ";
+    }
+  }
+
+  return cleanText;
 }
 
-console.log(cleanUp("---what's my +*& line?"));
+function lastChar(text) {
+  return text[text.length - 1];
+}
+
+function isLowerCaseLetter(char) {
+  return char >= "a" && char <= "z";
+}
+
+function isUpperCaseLetter(char) {
+  return char >= "A" && char <= "Z";
+}
+
+function cleanUp(text) {
+  return text.replace(/[^a-z]/gi, " ").replace(/\s+/gi, " ");
+}
